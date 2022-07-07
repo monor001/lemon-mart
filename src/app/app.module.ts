f3475f816d2a951f965f45881316dea4c3d37b5c
monor001 <eng.moh.nas@gmail.com>
2022-07-07 18:05:45 +0200

    Update for Lemon App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
+import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core'
 import { FlexLayoutModule } from '@angular/flex-layout'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { MaterialModule } from './material.module';
-import { ManagerModule } from './manager/manager.module';
-import { InventoryModule } from './inventory/inventory.module';
-import { PosModule } from './pos/pos.module';
+import { HomeComponent } from './home/home.component'
+import { InventoryModule } from './inventory/inventory.module'
+import { ManagerModule } from './manager/manager.module'
+import { MaterialModule } from './material.module'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+import { PosModule } from './pos/pos.module'
 import { UserModule } from './user/user.module'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MaterialModule, FlexLayoutModule, ManagerModule, InventoryModule, PosModule, UserModule],
+  declarations: [AppComponent, HomeComponent, PageNotFoundComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    FlexLayoutModule,
+    ManagerModule,
+    InventoryModule,
+    PosModule,
+    UserModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
