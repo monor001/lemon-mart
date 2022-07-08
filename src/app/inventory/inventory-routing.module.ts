87da57cdb015ea54aad2d9042215c7cee63c4121
monor001 <eng.moh.nas@gmail.com>
2022-07-08 16:43:40 +0200

    Created Inventory and sub-comonents with routing

--- src/app/inventory/inventory-routing.module.ts
+++ src/app/inventory/inventory-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
+import { CategoriesComponent } from './categories/categories.component'
 import { InventoryHomeComponent } from './inventory-home/inventory-home.component'
+import { InventoryComponent } from './inventory.component'
+import { ProductsComponent } from './products/products.component'
+import { StockEntryComponent } from './stock-entry/stock-entry.component'
 
 const routes: Routes = [
-  { path: '', redirectTo: '/inventory/home', pathMatch: 'full' },
-  { path: 'home', component: InventoryHomeComponent },
+  {
+    path: '',
+    component: InventoryComponent,
+    children: [
+      { path: '', redirectTo: '/inventory/home', pathMatch: 'full' },
+      { path: 'home', component: InventoryHomeComponent },
+      { path: 'stock-entry', component: StockEntryComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: 'categories', component: CategoriesComponent },
+    ],
+  },
 ]
 
 @NgModule({
--- src/app/inventory/inventory.module.ts
+++ src/app/inventory/inventory.module.ts
@@ -1,17 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+import { FlexLayoutModule } from '@angular/flex-layout'
 
-import { InventoryRoutingModule } from './inventory-routing.module';
-import { InventoryHomeComponent } from './inventory-home/inventory-home.component';
-
+import { MaterialModule } from '../material.module'
+import { CategoriesComponent } from './categories/categories.component'
+import { InventoryHomeComponent } from './inventory-home/inventory-home.component'
+import { InventoryRoutingModule } from './inventory-routing.module'
+import { InventoryComponent } from './inventory.component'
+import { ProductsComponent } from './products/products.component'
+import { StockEntryComponent } from './stock-entry/stock-entry.component'
 
 @NgModule({
   declarations: [
-    InventoryHomeComponent
+    InventoryHomeComponent,
+    StockEntryComponent,
+    ProductsComponent,
+    CategoriesComponent,
+    InventoryComponent,
   ],
-  imports: [
-    CommonModule,
-    InventoryRoutingModule
-  ]
+  imports: [CommonModule, InventoryRoutingModule, MaterialModule, FlexLayoutModule],
 })
-export class InventoryModule { }
+export class InventoryModule {}
