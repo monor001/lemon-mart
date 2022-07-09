24b59ef338b5332d4aa32713de02860b2b02eeb6
monor001 <eng.moh.nas@gmail.com>
2022-07-09 10:54:02 +0200

    Cleaned out unneccessary imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,8 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HomeComponent } from './home/home.component'
-import { InventoryModule } from './inventory/inventory.module'
 import { MaterialModule } from './material.module'
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
-import { PosModule } from './pos/pos.module'
-import { UserModule } from './user/user.module'
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, PageNotFoundComponent],
@@ -19,9 +16,6 @@ import { UserModule } from './user/user.module'
     AppRoutingModule,
     MaterialModule,
     FlexLayoutModule,
-    InventoryModule,
-    PosModule,
-    UserModule,
     HttpClientModule,
   ],
   providers: [],
